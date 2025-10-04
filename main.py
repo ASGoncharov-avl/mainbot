c2f79548faa9b63bc2f77cc5aa768a6c0405082c
xross <xrossx@mail.ru>
2025-10-04 18:30:16 +0300

    micro_changes

--- main.py
+++ main.py
@@ -224,6 +224,7 @@ while True:
             latest = df.iloc[-2]
             signal = latest['signal']
             bot.send_message(TELEGRAM_CHAT_ID, f"{now}: {signal}")
+            print(f"{now}: {signal}")
 
             if signal in ['buy', 'sell'] and can_enter_again(signal):
                 entry_price = latest['close']
