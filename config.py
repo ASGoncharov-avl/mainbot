fe46076ad984044fb0b9543aad952b63a8fa23bd
ASGoncharov-avl <alexandrgoncharov2015@ya.ru>
2025-10-21 10:17:31 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 symbol = "DOGEUSDT"
 interval = 5 # минуты
-bb_period = 50
+bb_period = 40
 bb_std = 1
-STOP_LOSS_PCT = 0.02
+STOP_LOSS_PCT = 0.01
 
 min_cluster = 3
-bull_quant = 0.58
-bear_quant = 0.43
+bull_quant = 0.75
+bear_quant = 0.25
 rsi = 60
 period = 30 # период расчетов в днях
 total_bars = int(60 / interval * 24 * period)
